65fc13ae2117e525bed254c95d868dab01cba6e0
romansenchenko <romansenchenko@yandex.ru>
2022-04-17 12:02:46 +0300

    messages at component

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,8 +1,41 @@
 import React from 'react';
 import s from './Dialogs.module.css';
+import { Link } from "react-router-dom";
+
+const DialogItem = (props) => {
+    let path = '/dialogs/' + props.id;
+
+    return <div className={s.dialog + ' ' + s.active}>
+        <Link to={path}>{props.name}</Link>
+    </div>
+
+}
+
+const Message = (props) => {
+    return <div className={s.message}>{props.message}</div>
+
+}
+
 
 const Dialogs = () => {
-    return <div>Dialogs</div>
+    return (
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <DialogItem name="Димаа" id="1"/>
+                <DialogItem name="Света" id="2"/>
+                <DialogItem name="Саша" id="3"/>
+                <DialogItem name="Оля" id="4"/>
+                <DialogItem name="Андрей" id="5"/>
+                <DialogItem name="Вадим" id="6"/>
+            </div>
+
+            <div className={s.messages}>
+                <Message message="Hi" />
+                <Message message="Как дела?" />
+                <Message message="Что делаешь?" />
+            </div>
+        </div>
+    )
 }
 
 export default Dialogs;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import s from './Navbar.module.css';
 
 const Navbar = () => {
-    return <nav className={s.nav}>
+  return <nav className={s.nav}>
     <div className={s.nav__logo}>
       <img alt='logo' src='https://cdn.logo.com/hotlink-ok/logo-social.png' />
     </div>
+    <div className={`${s.item}`}>
+      <Link to="/profile" className={({isActive})=>isActive? `${s.activeLink}`:' '}>Profile</Link>
+    </div>
     <div className={`${s.item} ${s.active}`}>
-      <a href="/profile">Profile</a>
+      <Link to="/dialogs" className={({isActive})=>isActive? `${s.activeLink}`:' '}>Messages</Link>
     </div>
     <div className={`${s.item}`}>
-      <a href="/dialogs">Messages</a>
-    </div>
-    <div className={`${s.item}`}>
-      <a>Photos</a>
+      <Link to="/messages" className={({isActive})=>isActive? `${s.activeLink}`:' '}>Photos</Link>
     </div>
   </nav>
 }
