3525b8c5e8e4e5d26ab3cc3177fb2ea90707aa20
romansenchenko <romansenchenko@yandex.ru>
2022-04-18 19:01:45 +0300

    move obj in index.js

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,41 +1,12 @@
 import React from 'react';
 import s from './Dialogs.module.css';
-import { Link } from "react-router-dom";
+import DialogItem from './DialogItem/DialogItem';
+import Message from './Message/Message';
 
-const DialogItem = (props) => {
-    let path = '/dialogs/' + props.id;
+const Dialogs = (props) => {
 
-    return <div className={s.dialog + ' ' + s.active}>
-        <Link to={path}>{props.name}</Link>
-    </div>
-
-}
-
-const Message = (props) => {
-    return <div className={s.message}>{props.message}</div>
-
-}
-
-
-const Dialogs = () => {
-
-    let dialogs = [
-        { id: 1, name: "Дима" },
-        { id: 2, name: "Света" },
-        { id: 3, name: "Саша" },
-        { id: 4, name: "Оjля" },
-        { id: 5, name: "Анjдрей" },
-        { id: 6, name: "Вадим" }
-    ]
-
-    let messages = [
-        { id: 1, messages: "Ппривет!" },
-        { id: 2, messages: "Каак дела?" },
-        { id: 3, messages: "Ччто делаешь?" }
-    ]
-
-    let dialogsElements = dialogs.map( d => <DialogItem name={d.name} id={d.id} /> ); 
-    let messagesElements = messages.map( m => <Message message={m.messages} /> );
+    let dialogsElements = props.dialogs.map( d => <DialogItem name={d.name} id={d.id} /> ); 
+    let messagesElements =props.messages.map( m => <Message message={m.messages} /> );
 
     return (
         <div className={s.dialogs}>
