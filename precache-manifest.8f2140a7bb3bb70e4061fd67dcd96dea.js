663d44b08fbaf63896a1f64ac5416334e4adccef
romansenchenko <romansenchenko@yandex.ru>
2022-09-07 20:10:44 +0300

    Updates

---
+++ precache-manifest.8f2140a7bb3bb70e4061fd67dcd96dea.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ee939ae1c2883ce30b6",
+    "url": "/social-network-react/static/css/main.76d14675.chunk.css"
+  },
+  {
+    "revision": "2ee939ae1c2883ce30b6",
+    "url": "/social-network-react/static/js/main.2ee939ae.chunk.js"
+  },
+  {
+    "revision": "4c96546f042afac0166d",
+    "url": "/social-network-react/static/css/1.98e55559.chunk.css"
+  },
+  {
+    "revision": "4c96546f042afac0166d",
+    "url": "/social-network-react/static/js/1.4c96546f.chunk.js"
+  },
+  {
+    "revision": "3677215276171d9f4817",
+    "url": "/social-network-react/static/css/2.94ab25d5.chunk.css"
+  },
+  {
+    "revision": "3677215276171d9f4817",
+    "url": "/social-network-react/static/js/2.36772152.chunk.js"
+  },
+  {
+    "revision": "7ae6b3414168338c2f48",
+    "url": "/social-network-react/static/js/3.7ae6b341.chunk.js"
+  },
+  {
+    "revision": "fa436005c3d8a10eef07",
+    "url": "/social-network-react/static/js/4.fa436005.chunk.js"
+  },
+  {
+    "revision": "f5d521d4cd6651623436",
+    "url": "/social-network-react/static/js/5.f5d521d4.chunk.js"
+  },
+  {
+    "revision": "d7d3b236da239eab4031",
+    "url": "/social-network-react/static/js/runtime~main.d7d3b236.js"
+  },
+  {
+    "revision": "faa1770df438c8adb7129ce627b56090",
+    "url": "/social-network-react/static/media/logo-icon.faa1770d.svg"
+  },
+  {
+    "revision": "aacd0af717367ea00d5b42d2f57a24d2",
+    "url": "/social-network-react/static/media/preloaderOrigin.aacd0af7.gif"
+  },
+  {
+    "revision": "11b3abdd2efe17ca4f5c127acd4b12e5",
+    "url": "/social-network-react/index.html"
+  }
+];
